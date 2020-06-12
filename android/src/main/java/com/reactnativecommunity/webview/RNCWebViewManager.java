6a69451f3bdd2b3000109ad380e7675bc78f20df
Witalo Benicio <contato@witalobenicio.com>
2020-06-12 11:54:33 -0300

    removing call to SSL error

--- android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
+++ android/src/main/java/com/reactnativecommunity/webview/RNCWebViewManager.java
@@ -798,44 +798,6 @@ public class RNCWebViewManager extends SimpleViewManager<WebView> {
           } else {
             handler.cancel();
           }
-
-          int code = error.getPrimaryError();
-          String failingUrl = error.getUrl();
-          String description = "";
-
-          // https://developer.android.com/reference/android/net/http/SslError.html
-          switch (code) {
-            case SslError.SSL_DATE_INVALID:
-              description = "The date of the certificate is invalid";
-              break;
-            case SslError.SSL_EXPIRED:
-              description = "The certificate has expired";
-              break;
-            case SslError.SSL_IDMISMATCH:
-              description = "Hostname mismatch";
-              break;
-            case SslError.SSL_INVALID:
-              description = "A generic error occurred";
-              break;
-            case SslError.SSL_MAX_ERROR:
-              description = "The number of different SSL errors.";
-              break;
-            case SslError.SSL_NOTYETVALID:
-              description = "The certificate is not yet valid";
-              break;
-            case SslError.SSL_UNTRUSTED:
-              description = "The certificate authority is not trusted";
-              break;
-            default:
-              description = "Unknown SSL Error";
-              break;
-          }
-
-          this.onReceivedError(
-            webView,
-            code,
-            description,
-            failingUrl);
         }
 
     @Override
